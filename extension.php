92ac46747149751c87c7d4c2c787ba4938f5afe6
Peter Epp <peter@teknocat.org>
2013-04-06 10:07:01 -0600

    Updated to version 2.2

--- extension.php
+++ extension.php
@@ -3,10 +3,11 @@
  * Extension for easy inclusion of Blueprint CSS.
  * 
  * @package Extensions
+ * @subpackage BluePrintCss
  * @author Peter Epp
  * @copyright Copyright (c) 2009 Peter Epp (http://teknocat.org)
  * @license GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.html)
- * @version 2.0
+ * @version 2.0 $Id: extension.php 13961 2011-08-08 16:25:51Z teknocat $
  **/
 class BluePrintCss extends AbstractExtension {
 	public function run() {
